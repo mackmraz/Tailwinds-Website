e24d3e75de777ca2309e551f0cf9139d2d489b57
mackmraz <mackmraz@gmail.com>
2022-12-12 09:41:03 -0500

    Add new section which include new-features.

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -7,10 +7,10 @@ const NewFeatures = ({ imgUrl, title, subtitle }) => (
         src={imgUrl}
         alt="icon"
         className="w-1/2 h-1/2 object-contain"
-      />
-      <h1>Title {title} </h1>
-      <p>{subtitle}</p>
-    </div>
+      />    
+      </div>
+      <h1 className="mt-[26px] font-bold text-[24px] leading-[30px] text-white">{title} </h1>
+      <p className="flex-1 mt-[16px] font-normal text-[18px] text-[#b0b0b0] leading-[32px]">{subtitle}</p>
   </div>
 );
 
--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -23,8 +23,8 @@ const WhatsNew = () => (
         <TypingText title="| What's New?" />
         <TitleText title={<>What's new about Metaversus?</>} />
         <div className="mt-[48px] flex flex-wrap justify-between gap-[24px]">
-          {NewFeatures.map((feature) => (
-            <NewSteps
+          {newFeatures.map((feature) => (
+            <NewFeatures
             key={feature}
             {...feature}
             />
